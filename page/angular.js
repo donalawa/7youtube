09bf836c592adffb4ebcdb42df78ebb515499a1c
donalawa <54797997+donalawa@users.noreply.github.com>
2019-12-19 15:10:15 +0100

    Update angular.js

--- page/angular.js
+++ page/angular.js
@@ -1,3 +1,4 @@
 document.querySelector('#angular').addEventListener('click',function(){
+    //bellow we want to set the url to angular .html when some body clicks on the angular collection
     window.location.href = 'videos/angular.html'
 })
